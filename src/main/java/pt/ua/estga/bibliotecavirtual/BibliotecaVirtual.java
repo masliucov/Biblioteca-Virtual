08859d9031ff4f487c1e1d1b758bbfafe6860d7f
arti <masliucov1@gmail.com>
2024-11-20 23:05:37 +0000

    Login e Register funcional

---
+++ src/main/java/pt/ua/estga/bibliotecavirtual/BibliotecaVirtual.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pt.ua.estga.bibliotecavirtual;
+
+/**
+ *
+ * @author arti
+ */
+public class BibliotecaVirtual {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/pt/ua/estga/bibliotecavirtual/DatabaseUtil.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pt.ua.estga.bibliotecavirtual;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ *
+ * @author arti
+ */
+public class DatabaseUtil {
+       public static Connection getConnection() {
+        Properties props = new Properties();
+        // Ajuste o caminho conforme a localização do arquivo
+        String path = "../database.properties/database.properties";
+        try (FileInputStream in = new FileInputStream(path)) {
+            props.load(in);
+        } catch (IOException e) {
+            System.out.println("Erro ao carregar o arquivo de propriedades: " + e.getMessage());
+            return null;
+        }
+
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Diretório de trabalho atual: " + System.getProperty("user.dir"));
+        try (Connection conn = getConnection()) {
+            if (conn != null) {
+                System.out.println("Conexão estabelecida com sucesso!");
+            } else {
+                System.out.println("Falha ao estabelecer a conexão.");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro durante a conexão com o banco de dados: " + e.getMessage());
+        }
+    }
+}
